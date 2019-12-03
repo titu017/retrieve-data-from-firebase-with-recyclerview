d49e4010a6f59ade96e1adfa6080312a2ff0da84
Titu <titu017@gmail.com>
2019-12-04 05:40:59 +0600

    branch3_mainactivity section is done and successfully run

--- app/src/main/java/com/softgather/books/MainActivity.java
+++ app/src/main/java/com/softgather/books/MainActivity.java
@@ -1,14 +1,44 @@
 package com.softgather.books;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    //step 1    define recyclerview
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //initialize
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerviewId);
+
+        //instanciate
+        new FirebaseDatabaseHelper().readBooks(new FirebaseDatabaseHelper.DataStatus() {
+            @Override
+            public void DataIsLoaded(List<Book> books, List<String> keys) {
+                new RecyclerView_Config().setConfig(recyclerView,MainActivity.this,books,keys);
+            }
+
+            @Override
+            public void DataIsInserted() {
+
+            }
+
+            @Override
+            public void DataIsUpdated() {
+
+            }
+
+            @Override
+            public void DataIsDeleted() {
+
+            }
+        });
     }
 }
